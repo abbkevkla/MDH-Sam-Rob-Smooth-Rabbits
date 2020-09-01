2904eced46b18cfbbaf534cc2c453674e76c7763
abbkevkla <43879859+abbkevkla@users.noreply.github.com>
2020-09-01 15:41:38 +0200

    .

--- Website/maze_script.js
+++ Website/maze_script.js
@@ -1,9 +1,45 @@
 const canvas = document.getElementById("myCanvas");
 const context = canvas.getContext("2d");
 
+document.onkeydown = checkKey; // Watching for keypresses, if one is pressed, run checkKey()
+function checkKey(e) { // e is the event, containing all the data of the keypress
+    if (e.key == "ArrowDown") {
+        console.log("down");
+    }
+    else if (e.key == "ArrowUp") {
+        /*if (maze.length < 6) {
+            steps = steps + 1;
+            current_pos[1] = current_pos[1] + 1;
+            maze.splice(current_pos[1] + 1, 0, steps);
+        } */
+        console.log("up");
+    }
+    else if (e.key == "ArrowLeft") {
+        /*if (maze[0].length < 6) {
+            steps = steps + 1;
+            current_pos[1] = current_pos[1] + 1;
+            maze[current_pos[1]].splice(current_pos[0], 0, steps);
+        } */
+        console.log("left");
+    }
+    else if (e.key == "ArrowRight") {
+        if (maze[0].length < 6) {
+            steps = steps + 1;
+            maze[current_pos[1]].splice(current_pos[0] + 1, 0, steps);
+            current_pos[0] = current_pos[0] + 1;
+        }
+        console.log("right");
+    }
+    console.log(maze);
+}
+
 context.fillStyle = "purple";
 context.fillRect(0, 0, 420, 420);
 context.fill();
+
+let maze = [["x", 0, 0]];
+let current_pos = [0, 0];
+let steps = 0;
 
 let size = 6 // Maze is 6x6 squares
 let dist_x = 0;
