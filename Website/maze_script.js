5369e85fc3d573580a1b374c80f3c84d299ee174
Minowtf <44235674+Minowtf@users.noreply.github.com>
2020-09-01 20:10:22 +0200

    move down/right
    Can now move down and right with the map dynamically adapting to fit

--- Website/maze_script.js
+++ Website/maze_script.js
@@ -4,29 +4,46 @@ const context = canvas.getContext("2d");
 document.onkeydown = checkKey; // Watching for keypresses, if one is pressed, run checkKey()
 function checkKey(e) { // e is the event, containing all the data of the keypress
     if (e.key == "ArrowDown") {
-        console.log("down");
-    }
-    else if (e.key == "ArrowUp") {
-        /*if (maze.length < 6) {
+        if (maze.length < 6) {
             steps = steps + 1;
             current_pos[1] = current_pos[1] + 1;
-            maze.splice(current_pos[1] + 1, 0, steps);
-        } */
+            current_height = current_height + 1;
+
+            if (maze[current_pos[1]] && maze[current_pos[1]][current_pos[0]] == 0) {
+                maze[current_pos[1]][current_pos[0]] = steps;
+            } 
+            else {
+                let new_row = [];
+                for (i = 0; i < current_width; i++) {
+                    new_row.push(0);
+                }
+                new_row[current_pos[0]] = steps;
+                maze.push(new_row);
+            } 
+        console.log("down");
+        }
+    }
+    else if (e.key == "ArrowUp") {
         console.log("up");
     }
     else if (e.key == "ArrowLeft") {
-        /*if (maze[0].length < 6) {
-            steps = steps + 1;
-            current_pos[1] = current_pos[1] + 1;
-            maze[current_pos[1]].splice(current_pos[0], 0, steps);
-        } */
         console.log("left");
     }
     else if (e.key == "ArrowRight") {
         if (maze[0].length < 6) {
             steps = steps + 1;
-            maze[current_pos[1]].splice(current_pos[0] + 1, 0, steps);
-            current_pos[0] = current_pos[0] + 1;
+            current_pos[0] = current_pos[0] + 1; 
+            current_width = current_width + 1;
+
+            if (maze[current_pos[1]][current_pos[0]] == 0) {
+                maze[current_pos[1]][current_pos[0]] = steps;
+            } 
+            else {
+                for (i = 0; i < current_height; i++) {
+                    maze[i].push(0);
+                }
+                maze[current_pos[1]][current_pos[0]] = steps;
+            }
         }
         console.log("right");
     }
@@ -37,9 +54,13 @@ context.fillStyle = "purple";
 context.fillRect(0, 0, 420, 420);
 context.fill();
 
-let maze = [["x", 0, 0]];
+let maze = [
+    ["x"]
+];
 let current_pos = [0, 0];
 let steps = 0;
+let current_width = 1;
+let current_height = 1;
 
 let size = 6 // Maze is 6x6 squares
 let dist_x = 0;
