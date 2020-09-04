c984c4290383816829ca0e4bc1c7d3381f9d1b48
abbkevkla <43879859+abbkevkla@users.noreply.github.com>
2020-09-04 11:24:00 +0200

    Visualize map

--- Website/maze_script.js
+++ Website/maze_script.js
@@ -4,50 +4,102 @@ const context = canvas.getContext("2d");
 document.onkeydown = checkKey; // Watching for keypresses, if one is pressed, run checkKey()
 function checkKey(e) { // e is the event, containing all the data of the keypress
     if (e.key == "ArrowDown") {
-        if (maze.length < 6) {
+        if (current_pos[1] < size_y - 1) {
+            console.log("down");
             steps = steps + 1;
             current_pos[1] = current_pos[1] + 1;
-            current_height = current_height + 1;
+            relative_pos[1] = relative_pos[1] + 1;
 
-            if (maze[current_pos[1]] && maze[current_pos[1]][current_pos[0]] == 0) {
-                maze[current_pos[1]][current_pos[0]] = steps;
-            } 
-            else {
+            if (typeof maze[relative_pos[1]] == "undefined") {
+                current_height = current_height + 1;
                 let new_row = [];
                 for (i = 0; i < current_width; i++) {
                     new_row.push(0);
                 }
-                new_row[current_pos[0]] = steps;
-                maze.push(new_row);
+                new_row[relative_pos[0]] = steps;
+                maze.push(new_row);   
             } 
-        console.log("down");
+            else {
+                maze[relative_pos[1]][relative_pos[0]] = steps;
+            } 
+        } 
+        else {
+            console.log("can't go down");
         }
     }
     else if (e.key == "ArrowUp") {
-        console.log("up");
-    }
-    else if (e.key == "ArrowLeft") {
-        console.log("left");
-    }
-    else if (e.key == "ArrowRight") {
-        if (maze[0].length < 6) {
+        if (current_pos[1] > 0) {
+            console.log("up");
             steps = steps + 1;
-            current_pos[0] = current_pos[0] + 1; 
-            current_width = current_width + 1;
+            current_pos[1] = current_pos[1] - 1;
 
-            if (maze[current_pos[1]][current_pos[0]] == 0) {
-                maze[current_pos[1]][current_pos[0]] = steps;
+            if (typeof maze[relative_pos[1] - 1] == "undefined") {
+                current_height = current_height + 1;
+                let new_row = [];
+                for (i = 0; i < current_width; i++) {
+                    new_row.push(0);
+                }
+                new_row[relative_pos[0]] = steps;
+                maze.unshift(new_row);   
             } 
             else {
+                relative_pos[1] = relative_pos[1] - 1;
+                maze[relative_pos[1]][relative_pos[0]] = steps;
+            } 
+        } 
+        else {
+            console.log("can't go up");
+        }
+    }
+    else if (e.key == "ArrowLeft") {
+        if (current_pos[0] > 0) {
+            console.log("left");
+            current_pos[0] = current_pos[0] - 1;
+            steps = steps + 1;
+            if (typeof maze[relative_pos[1]][relative_pos[0] - 1] == "undefined") {
+                current_width = current_width + 1;
+                for (i = 0; i < current_height; i++) {
+                    maze[i].unshift(0);
+                }
+                maze[relative_pos[1]][0] = steps;
+            }
+            else {
+                relative_pos[0] = relative_pos[0] - 1;
+                maze[relative_pos[1]][relative_pos[0]] = steps;
+            }
+        }
+        else {
+            console.log("can't go left");
+        }
+    }
+    else if (e.key == "ArrowRight") {
+        if (current_pos[0] < size_x - 1) {
+            console.log("right");
+            steps = steps + 1;
+            current_pos[0] = current_pos[0] + 1; 
+            relative_pos[0] = relative_pos[0] + 1;
+            if (typeof maze[relative_pos[1]][relative_pos[0]] == "undefined") {
+                current_width = current_width + 1;
                 for (i = 0; i < current_height; i++) {
                     maze[i].push(0);
                 }
-                maze[current_pos[1]][current_pos[0]] = steps;
-            }
+            } 
+            maze[relative_pos[1]][relative_pos[0]] = steps;
         }
-        console.log("right");
+        else {
+            console.log("can't go right");
+        }
     }
     console.log(maze);
+    console.log(current_pos);
+    context.beginPath();
+    context.fillStyle = "yellow";
+    context.strokeStyle = "black";
+    context.lineWidth = "3";
+    context.rect(current_pos[0] * tile_w, current_pos[1] * tile_h, tile_w, tile_h);
+    context.fillRect(current_pos[0] * tile_w, current_pos[1] * tile_h, tile_w, tile_h);
+    context.fill();
+    context.stroke();
 }
 
 context.fillStyle = "purple";
@@ -57,19 +109,21 @@ context.fill();
 let maze = [
     ["x"]
 ];
+let relative_pos = [0, 0];
 let current_pos = [0, 0];
 let steps = 0;
 let current_width = 1;
 let current_height = 1;
 
-let size = 6 // Maze is 6x6 squares
+let size_x = 6; // Maze is 6x6 squares
+let size_y = 6;
 let dist_x = 0;
 let dist_y = 0;
-let tile_w = canvas.width/size; // Tilesize adapts to fit the given canvas
-let tile_h = canvas.height/size;
+let tile_w = canvas.width/size_x; // Tilesize adapts to fit the given canvas
+let tile_h = canvas.height/size_y;
 
-for (r = 0; r < size; r++) { 
-    for (c = 0; c < size; c++) {
+for (r = 0; r < size_x; r++) { 
+    for (c = 0; c < size_y; c++) {
         context.beginPath();
         context.strokeStyle = "black";
         context.lineWidth = "3";
@@ -80,3 +134,4 @@ for (r = 0; r < size; r++) {
     dist_x = 0;
     dist_y = dist_y + tile_h; 
 }
+dist_y = 0;
